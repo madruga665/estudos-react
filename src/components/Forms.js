523a5b796e5a3e8bce222629051a4b50d1c933e3
madruga665 <madruga665@hotmail.com>
2021-05-01 14:57:27 -0300

    add metodos handleChange e handleSubmit

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,29 +2,46 @@ import React from "react";
 import "./styles/Forms.css";
 
 class Forms extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "", email: "", password: "" };
+  }
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.name);
+  }
   render() {
     return (
       <div>
         <h1>Cadastro</h1>
         <form className="my-form">
           <div className="input-group">
-            <label for="name">Nome: </label>
-            <input id="name" type="text" />
+            <label htmlFor="name">Nome: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
             <span></span>
           </div>
 
           <div className="input-group">
-            <label for="email">Email: </label>
+            <label htmlFor="email">Email: </label>
             <input id="email" type="email" />
             <span></span>
           </div>
 
           <div className="input-group">
-            <label for="password">Senha: </label>
+            <label htmlFor="password">Senha: </label>
             <input id="password" type="password" />
             <span></span>
           </div>
-          <button type="submit">Enviar</button>
+          <button type="submit" onClick={this.handleSubmit}>Enviar</button>
         </form>
       </div>
     );
