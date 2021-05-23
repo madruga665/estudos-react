3d10137776b4c4be99e44422d3e135182b6dccf6
madruga665 <madruga665@hotmail.com>
2021-05-23 11:25:59 -0300

    Coloca o input dentro de um form

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -6,32 +6,38 @@ class InputText extends React.Component {
     this.state = {
       text: "",
     };
-    this.handleClick = this.handleClick.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.alertOnSubmit = this.alertOnSubmit.bind(this);
   }
-  handleClick() {
-    this.Submit();
+  handleClick(event) {
+    event.preventDefault();
+    this.alertOnSubmit();
+    this.setState({ text: "" });
   }
-  handleChange (event) {
-    this.setState ({
-      text: event.target.value
-    })
+  handleChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
   }
-  Submit () {
-    alert(`aqui esta o que vc digitou: ${this.state.text}`)
+  alertOnSubmit() {
+    alert(`aqui esta o que vc digitou: \n ${this.state.text}`);
   }
   render() {
     return (
       <div>
-        <input
-          style={{ marginTop: "50px" }}
-          placeholder="digite alguma coisa"
-          value={this.state.text}
-          type="text"
-          onChange={this.handleChange}
-        >
-        </input>
-        <button onClick={this.handleClick}>Enviar</button>
+        <form>
+          <input
+            style={{ marginTop: "50px" }}
+            placeholder="digite alguma coisa"
+            value={this.state.text}
+            type="text"
+            onChange={this.handleChange}
+          ></input>
+          <button type="submit" onClick={this.handleClick}>
+            Enviar
+          </button>
+        </form>
       </div>
     );
   }
