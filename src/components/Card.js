4a7c45f560448478860fd29e0d7666a04eb2841f
madruga665 <madruga665@hotmail.com>
2021-06-08 13:39:21 -0300

    add animations

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./styles/Card.css";
+import './styles/Animations.css';
 
 class Card extends React.Component {
   render() {
     const { titulo, color, children } = this.props;
     return (
-      <div className="card" style={{ borderColor: color }}>
+      <div className="card animate-up" style={{ borderColor: color }}>
         <div className="card-content">{children}</div>
         <footer className="card-footer" style={{ backgroundColor: color }}>
           {titulo}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./styles/Header.css"
+import './styles/Animations.css';
 
 class Header extends React.Component {
   render() {
     return (
-      <header className="header" >
+      <header className="header animate-appear" >
         <h1>Estudando Front-end na Trybe</h1>
       </header>
     );
